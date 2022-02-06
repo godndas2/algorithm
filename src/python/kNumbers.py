1a32f9a985b925b15baa44fbab9d74b02b695066
halfdev <gusgodnd@naver.com>
2022-02-06 23:32:43 +0900

    kNumbers

---
+++ src/python/kNumbers.py
@@ -0,0 +1,20 @@
+# k번 째 수
+
+# input
+# 2
+# 6 2 5 3
+# 5 2 7 3 8 9
+# 15 3 10 3
+# 4 15 8 16 6 6 17 3 10 11 18 7 14 7 15
+# ouput
+# 1 7
+# 2 6
+
+T = int(input())
+
+for t in range(T):
+    n, s, e, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = a[s-1:e]
+    a.sort()
+    print("#%d %d" %(t+1, a[k-1]))
