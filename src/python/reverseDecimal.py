a7f6d90858c7e053f3e393e69095be78fde95b21
halfdev <gusgodnd@naver.com>
2022-02-09 22:28:53 +0900

    reverseDecimal

---
+++ src/python/reverseDecimal.py
@@ -0,0 +1,34 @@
+# input
+# 5
+# 32 55 62 3700 250
+# output
+# 23 73
+
+from audioop import reverse
+from cgitb import text
+
+def reverse(x):
+    res = 0
+    while x > 0:
+        t = x % 10
+        res = res * 10 + t
+        x = x // 10
+
+    return res
+
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2, x // 2 + 1):
+        if x % i == 0:
+            return False
+    else:
+        return True
+
+N = int(input())
+a = list(map(int, input().split()))
+
+for x in a:
+    tmp = reverse(x)
+    if isPrime(tmp):
+        print(tmp, end = ' ')
