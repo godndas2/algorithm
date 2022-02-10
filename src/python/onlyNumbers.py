7fcade8dde161ed36f6223aa6c9d19136ecd94fd
halfdev <gusgodnd@naver.com>
2022-02-10 23:10:06 +0900

    숫자만 추출

---
+++ src/python/onlyNumbers.py
@@ -0,0 +1,21 @@
+# input
+# gsfefs2ewrwd8
+# output
+# 28
+
+S = input
+res = 0
+
+for x in S:
+    if x.isdecimal():
+        res = res * 10 + int(x)
+
+print(res)
+
+cnt = 0
+
+for i in range(1, res + 1):
+    if res % i == 0:
+        cnt += 1
+
+print(cnt)
