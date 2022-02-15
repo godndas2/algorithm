4de3ffc2bacab4250c1f78c29dbab24b33c61b54
halfdev <gusgodnd@naver.com>
2022-02-15 22:52:47 +0900

    refactoring

--- src/python/dictionaryHash.py
+++ src/python/dictionaryHash.py
@@ -5,20 +5,15 @@
 # YES
 a = input()
 b = input()
-str1 = dict()
-str2 = dict()
+sh = dict()
 
 for x in a:
-    str1[x] = str1.get(x, 0) + 1
+    sh[x] = sh.get(x, 0) + 1
 for x in b:
-    str2[x] = str2.get(x, 0) + 1
+    sh[x] = sh.get(x, 0) - 1
 
-for i in str1.keys():
-    if i in str2.keys():
-        if str1[i] != str2[i]:
-            print("NO")
-            break
-    else:
+for x in a:
+    if sh.get(x) > 0:
         print("NO")
         break
 else:
